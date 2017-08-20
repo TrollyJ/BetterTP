12e94617b2420fdaf399a9bceef9024fcaf87fce
TrollyJ <petrik36@post.sk>
2017-08-20 21:36:36 +0200

    Added teleport and back feature.

--- src/BetterTP/BetterTP.java
+++ src/BetterTP/BetterTP.java
@@ -10,26 +10,72 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class BetterTP extends JavaPlugin {
     
+    Location tpFrom = null;
+    
     public static final Logger LOG = Logger.getLogger("Minecraft");
     
     @Override
     public boolean onCommand (CommandSender sender, Command command, String navesti, String[] arguments) {
         if (navesti.equalsIgnoreCase("btp")) {
             if (sender instanceof Player) {
-                doBTP(sender);
-                return true;
+                if (arguments.length > 0) {
+                    String playerToTp = arguments[0];
+                    doBTP(sender, playerToTp);
+                    return true;
+                } else {
+                    BTPNoPlayer(sender);
+                }
+            }
+        }
+        if (navesti.equalsIgnoreCase("back")) {
+            if (sender instanceof Player) {
+                back(sender);
             }
         }
         return false;
     }
     
-    public void doBTP (CommandSender sender) {
-        Player player = (Player) sender;
+    public void doBTP (CommandSender sender, String playerToTP) {
+        Player player = (Player) sender; 
+        Player playerToTp = getServer().getPlayer(playerToTP);
+        Location playerToTpLoc = playerToTp.getLocation();
         Location location = player.getLocation();
         World world = player.getWorld();
+        double x = playerToTpLoc.getX();
+        double y = playerToTpLoc.getY();
+        double z = playerToTpLoc.getZ();
         
+        try {
+        player.teleport(playerToTpLoc);
+        } catch (Exception e) {
+            player.sendMessage("This player is not online");
+        }
         
-        LOG.info("[BetterTP] Teleported to ");
+        tpFrom = location;
+        
+        LOG.info("[BetterTP] Teleported to " + x + " " + y + "" + z);
+    }
+    
+    public void back (CommandSender sender) {
+        Player player = (Player) sender;
+        try {
+        player.teleport(tpFrom);
+        } catch (Exception e) {
+            player.sendMessage("You have not teleported yet.");
+        }
+        
+        Location loc = player.getLocation();
+        
+        double x = loc.getX();
+        double y = loc.getY();
+        double z = loc.getZ();
+        
+        LOG.info("[BetterTP] Teleported back to " + x + " " + y + " " + z);
+    }
+    
+    public void BTPNoPlayer (CommandSender sender) {
+        Player player = (Player) sender;
+        player.sendMessage("Specify player to teleport to.");
     }
     
 }
