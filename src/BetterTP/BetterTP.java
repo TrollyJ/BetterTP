6780c40dfd591f08d97dec4838a1c23cddecefa3
TrollyJ <petrik36@post.sk>
2017-08-21 09:13:42 +0200

    Logger now shows which player was teleported. Plugin was tested and works.

--- src/BetterTP/BetterTP.java
+++ src/BetterTP/BetterTP.java
@@ -30,6 +30,7 @@ public class BetterTP extends JavaPlugin {
         if (navesti.equalsIgnoreCase("back")) {
             if (sender instanceof Player) {
                 back(sender);
+                return true;
             }
         }
         return false;
@@ -37,6 +38,7 @@ public class BetterTP extends JavaPlugin {
     
     public void doBTP (CommandSender sender, String playerToTP) {
         Player player = (Player) sender; 
+        try {
         Player playerToTp = getServer().getPlayer(playerToTP);
         Location playerToTpLoc = playerToTp.getLocation();
         Location location = player.getLocation();
@@ -45,32 +47,34 @@ public class BetterTP extends JavaPlugin {
         double y = playerToTpLoc.getY();
         double z = playerToTpLoc.getZ();
         
-        try {
+        String playerSt = player.getName();
+        
         player.teleport(playerToTpLoc);
-        } catch (Exception a) {
-            player.sendMessage("This player is not online");
-        }
         
         tpFrom = location;
         
-        LOG.info("[BetterTP] Teleported to " + x + " " + y + "" + z);
+        LOG.info("[BetterTP] Teleported " + playerSt + " to " + x + " " + y + "" + z);
+        } catch (Exception e) {
+         player.sendMessage("Player is not online");
+         LOG.info("[BetterTP] Player is not onlline");
+        }
     }
     
     public void back (CommandSender sender) {
         Player player = (Player) sender;
+        String playerSt = player.getName();
         try {
+        
         player.teleport(tpFrom);
         } catch (Exception e) {
             player.sendMessage("You have not teleported yet.");
         }
         
-        Location loc = player.getLocation();
+        double x = tpFrom.getX();
+        double y = tpFrom.getY();
+        double z = tpFrom.getZ();
         
-        double x = loc.getX();
-        double y = loc.getY();
-        double z = loc.getZ();
-        
-        LOG.info("[BetterTP] Teleported back to " + x + " " + y + " " + z);
+        LOG.info("[BetterTP] Teleported " + playerSt + " back to " + x + " " + y + " " + z);
     }
     
     public void BTPNoPlayer (CommandSender sender) {
